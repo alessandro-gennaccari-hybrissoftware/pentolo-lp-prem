211a7b3eedccdeaeb4254c535a56d9b36669f806
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2026-03-16 16:24:49 +0100

    Refactor Navbar component and localization files by removing unused 'example' entry

--- src/ui/organisms/Navbar/Navbar.tsx
+++ src/ui/organisms/Navbar/Navbar.tsx
@@ -1,30 +1,27 @@
 // Libraries
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl"
 // Components
-import { NavigationLink, LanguageDropdown } from '@/ui';
+import { NavigationLink, LanguageDropdown } from "@/ui"
 
 const Navbar = () => {
-	const t = useTranslations('Navigation');
+  const t = useTranslations("Navigation")
 
-	return (
-		<div className='bg-white border-b p-2 flex items-center justify-between'>
-			{/* Logo */}
-			<div>Logo</div>
+  return (
+    <div className="bg-white border-b p-2 flex items-center justify-between">
+      {/* Logo */}
+      <div>Logo</div>
 
-			<div className='flex items-center gap-x-8'>
-				{/* Nav */}
-				<nav className='flex items-center gap-x-4'>
-					<NavigationLink href='/'>{t('home')}</NavigationLink>
-					<NavigationLink href='/example'>
-						{t('example')}
-					</NavigationLink>
-				</nav>
+      <div className="flex items-center gap-x-8">
+        {/* Nav */}
+        <nav className="flex items-center gap-x-4">
+          <NavigationLink href="/">{t("home")}</NavigationLink>
+        </nav>
 
-				{/* Change Language */}
-				<LanguageDropdown />
-			</div>
-		</div>
-	);
-};
+        {/* Change Language */}
+        <LanguageDropdown />
+      </div>
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
